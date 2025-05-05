d77be3e0f934d6a800be3e23f3a5b80ddff0b902
Methyx <methyx95@gmail.com>
2025-05-05 14:49:00 +0200

    add kwattsColor

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get("/allo", async (req, res) => {
   res.json("coucou mes amis");
 });
 
+app.get("/getkWattsColor", async (req, res) => {
+  try {
+    const key = "8055ab6b8560796d19caf2510a878ac3"; // philippe.mercy@free.fr on DEV
+    const response = await axios.get(
+      "https://dev01.cumkwatt.com/api_front?key=" + key
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 app.all("*", (req, res) => {
   res.status(401).json({ message: "route not found" });
 });
